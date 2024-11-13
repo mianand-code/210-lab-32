0a7b789dbb92c769bdfc84e97c405b28c637a50d
Mira Anand <miraanand@comcast.net>
2024-11-13 11:34:13 -0800

    10 minute update - 1

--- mainMiraAnand.cpp
+++ mainMiraAnand.cpp
@@ -1,3 +1,31 @@
 // COMSC-210 | Lab 32 | Mira Anand
 // Module 12, Lesson: std::deque & std::stack, Assignment: Toll Booth
 // IDE used: Visual Studio Code for Mac
+
+#include "Car.h" // references the Car header file, which contains a class to create Car objects
+#include <cstdlib> // needed to generate a random #
+#include <ctime> // needed to generate a random #
+#include <deque> // needed to use std::deque
+#include <iostream>
+using namespace std;
+
+// declaration and initialization of global const int variables
+const int INITIAL_LINE = 2; // represents the # of cars that are already in line to pay the toll when the simulation starts
+const int HEAD_CAR_PAYS_LEAVES = 55; // represents the probability that the car at the head of the line pays its toll and leaves the toll booth
+const int CAR_JOINS = 45; // represents the probability that another car will join the line for the toll booth
+
+int main()
+{
+    srand(time(0)); // needed as the first line in main() in order to generate a random # for probability
+
+    deque<Car> tollBoothLine; // create an std::deque that will hold Car objects
+
+    int probability = rand() % 100 + 1; // generates a random # between 1-100 for probability
+    int timePeriod = 1; // to keep track of the current time period
+
+    // when the simulation starts, 2 cars should already be in line to pay the toll
+    for (int i = 0; i < INITIAL_LINE; i++)
+        tollBoothLine.push_back(Car()); // using .push_back to add a Car object (using the Car class) to the back of the deque
+    
+    return 0;
+}
