f9baf3bab2a72709029b72ec1999e083af06576e
Mira Anand <miraanand@comcast.net>
2024-11-16 10:53:14 -0800

    Final commit - Finishing lab #33, final edits were made

--- mainMiraAnand.cpp
+++ mainMiraAnand.cpp
@@ -14,16 +14,16 @@ using namespace std;
 
 // declaration and initialization of global const int variables
 const int TOLL_BOOTH_LANE_NUM = 4; // represents the # of toll booth lanes in the plaza
-const int INITIAL_LINE_MAX = 3; // represents the maximum # of cars that can join the initial line, will be used to generate a random # of cars between 1-3
+const int INITIAL_LINE_MAX = 3; // represents the maximum # of cars that can join the initial line, will be used to generate a random # of cars between 1-3 in main()
 const int HEAD_CAR_PAYS_LEAVES = 46; // represents the probability that the car at the head of the line pays its toll and leaves the toll booth
 const int CAR_JOINS = 39; // represents the probability that another car will join the line for the toll booth
-const int REAR_CAR_SWITCHES = 15; // represents the probability that the rear car will switch lanes
-const int CAR_JOINS_EMPTY_LANE = 50; // represents the probability that a new car will join the lane if it is completely empty and there is still more time in the simulation
+const int REAR_CAR_SWITCHES = 15; // represents the probability that a rear car will switch lanes
+const int CAR_JOINS_EMPTY_LANE = 50; // represents the probability that a new car will join a lane if it is completely empty and there is still more time in the simulation
 const int TIME_PERIODS = 20; // represents the # of time periods the simulation will run for
 
 int main()
 {
-    srand(time(0)); // needed as the first line in main() in order to generate a random # for probability
+    srand(time(0)); // needed as the first line in main() for randomization purposes
 
     // create an array of type deque<Car>
     // will be used to represent the multiple toll booth lanes (4) that each hold their own Car objects
@@ -49,8 +49,8 @@ int main()
     {
         cout << "Lane: " << i + 1 << ": " << endl; // output the lane #
         
-        // use a range-based for loop and the "auto" keyword to output the Car objects in each of the 4 lanes
-        // loop through the array of type deque<Car>
+        // use a range-based for loop and the "auto" keyword to output the Car objects in each one of the 4 lanes
+        // traverse through the array of type deque<Car>
         for (auto car : tollBoothLanes[i]) // [i] accesses a specific lane (array index)
         {
             cout << "    ";
@@ -63,20 +63,21 @@ int main()
         cout << endl;
         cout << "Time: " << timePeriod << endl; // output the current time period
 
-        for (int i = 0; i < TOLL_BOOTH_LANE_NUM; i++) // creation of another for loop to handle each of the lanes
+        for (int i = 0; i < TOLL_BOOTH_LANE_NUM; i++) // creation of another inner for loop to handle each one of the lanes
         {
             // creating 3 if statements so that probability events can be handled independently from one another
-            // this means that during a time period, there is a possibility that all events can occur - a head car leaving, a car joining, and a rear car switching lanes, which is an accurate situation for a toll booth
-            // if a car joins the end of the line and that same car switches lanes in the same time period, this can mimic a situation where the car realizes that it may be better off in a different line that is being processed faster
-            // there is also a possibility that no events occur during a time period - no car leaves, no car joins, and no car switches
-            // this mimics a situation where a car may not leave because there is a delay when processing payment at the toll booth and a car simply may not join because there are no incoming cars. Also, no car may want/need to switch
+            // this means that during a time period, there is a possibility that all events can occur - a head car leaving, a new car joining, and a rear car switching lanes, which is an accurate situation for a toll booth
+            // if a new car joins the end of one line and that same car decides to switch lanes in the same time period, this can mimic a situation where the car realizes that it may be better off in a different line that is being processed faster
+            // - or the line they have switched into is not operational at the moment and they need to immediately switch to a different one
+            // there is also a possibility that no events occur during a time period - no head car leaves, no new car joins, and no rear car switches
+            // - this mimics a situation where a head car may not leave because there is a delay when processing payment at the toll booth and a new car simply may not join because there are no incoming cars. Also, no rear car may want/need to switch
             
             // handle the case where the car at the head of the line pays & leaves
-            // use .empty() to ensure the lane is not empty before proceeding with head car removal
+            // use .empty() to ensure that the lane is not empty before proceeding with head car removal
             if (!tollBoothLanes[i].empty() && probability <= HEAD_CAR_PAYS_LEAVES)
             {
                 // I am displaying the car's information first before using .pop_back() so that I can access the car's information directly before removing it from the deque
-                // this is valid because the for statement above performs a check to ensure that we will not remove from an empty deque
+                // this is valid because the if statement performs a check to ensure that we will not remove from an empty deque
                 cout << "Lane: " << i + 1 << " Paid: ";
                 // [i] is used to access a specific lane within the array
                 tollBoothLanes[i].front().print(); // access the information of the head car by using .front() & print its info using .print() from the Car class
@@ -84,8 +85,7 @@ int main()
             }
 
             // handle the case where another car joins the line
-            // will also handle the situation in which the line is empty, but there is still more time in the simulation
-            // there will be a 50/50 probability of a new car joining the line if the line is completely empty
+            // will also handle the case in which the line is empty, but there is still more time in the simulation - there will be a 50/50 probability of a new car joining the line if the line is completely empty
             if (tollBoothLanes[i].empty()) // check if the lane is empty
             {
                 probability = rand() % 100 + 1; // generate probability for the case
@@ -107,13 +107,13 @@ int main()
                 }
             }
 
-            // handle the case where a rear car will shift lanes to a random lanes that is not their own
+            // handle the case where a rear car will shift lanes to a random lane that is not their own
             probability = rand() % 100 + 1; // generate probability for this case
-            // use .empty() to ensure the lane is not empty before proceeding with the switching case
+            // use .empty() to ensure that the lane is not empty before proceeding with switching of a rear car
             if (!tollBoothLanes[i].empty() && probability <= REAR_CAR_SWITCHES)
             {
                 int randomLane; // creation of a variable to hold the random lane # that the car will switch into
-                // create a do-while loop so that a random lane # will be generated until a lane number is generated that does not match the one the car is currently in
+                // create a do-while loop so that a random lane # will be generated until one is generated that does not match the one the car is currently in
                 do
                 {
                     randomLane = rand() % TOLL_BOOTH_LANE_NUM;
@@ -142,7 +142,7 @@ int main()
             }
             else // display the cars in the lane's line
             {
-                for (auto car : tollBoothLanes[i]) // use a range-based for loop and the "auto" keyword to output the Car objects in each of the 4 lanes
+                for (auto car : tollBoothLanes[i]) // use a range-based for loop and the "auto" keyword to output the Car objects in each one of the 4 lanes
                 {
                     cout << "    ";
                     car.print(); // using the .print() nethod in the Car class to print each Car object stored in the deque
